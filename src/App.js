c9d64bd11bbece943642722fb057148e57476f7b
Jurij Zakharov <amourphys@gmail.com>
2021-12-14 17:48:07 +0200

    create 2 components in folder - components:Counter.jsx & ClassCounter.jsx; create new folder - styles and make in file: App.css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
+import ClassCounter from './components/ClassCounter';
+import './styles/App.css';
 
 function App() {
 
-  const [likes, setLikes] = useState(5)
-
-  function increment() {
-    setLikes(likes + 1)
-  }
-
-  function decrement() {
-    setLikes(likes - 1)
-  }
+ 
 
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+     <ClassCounter />
     </div>
   );
 }
