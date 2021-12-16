fc538a79db889b89a471ebbd47780e9dcb1d60a2
Jurij Zakharov <amourphys@gmail.com>
2021-12-16 21:56:40 +0200

    modified files: App.js & App.css; create 4 new directories - modal, hooks, API, loader;in hooks create new files: useFetching.js, usePosts.js; in modal: MyModal.jsx, MyModal.module.css; in API: PostService.js; in loader: Loader.jsx, Loader.module.css; also in directory components create new file: PostFilter.jsx

--- src/App.js
+++ src/App.js
@@ -1,68 +1,64 @@
-import React, { useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ClassCounter from './components/ClassCounter';
+import PostFilter from './components/PostFilter';
 import PostForm from './components/PostForm';
 import PostList from './components/PostList';
-import MyInput from './components/UI/input/MyInput';
-import MySelect from './components/UI/select/MySelect';
+import MyButton from './components/UI/button/MyButton';
+import MyModal from './components/UI/modal/MyModal';
+import { usePosts } from './hooks/usePosts';
 import './styles/App.css';
+import PostService from './API/PostService';
+import Loader from './loader/Loader';
 
 function App() {
   const [posts, setPosts] = useState([
-    {id: 1, title: 'Javascript', body: 'Description'},
+   /*  {id: 1, title: 'Javascript', body: 'Description'},
     {id: 2, title: 'JS', body: 'Descrip'},
-    {id: 3, title: 'Typescript', body: 'Des'}
+    {id: 3, title: 'Typescript', body: 'Des'} */
   ])
+  const [filter, setFilter] = useState({sort: '', query: ''})
+  const [modal, setModal] = useState(false)
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+  const [isPostsLoading, setIsPostsLoading] = useState(false)
 
-  const [selectedSort, setSelectedSort] = useState('')
-  const [searchQuery, setSearchQuery] = useState('')
+  useEffect(() => {
+    fetchPosts()
+  }, [])
 
-  const sortedPosts = useMemo(() => {
-    console.log('function complete')
-     if(selectedSort) {
-      return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
-    }
-    return posts; 
-  }, [selectedSort, posts])
-
-  const sortedAndSearchedPosts = useMemo(() => {
-      return sortedPosts.filter(post => post.title.includes(searchQuery))
-  }, [searchQuery, sortedPosts])
-
+  async function fetchPosts() {
+    setIsPostsLoading(true)
+    const posts = await PostService.getAll()
+    setPosts(posts)
+    setIsPostsLoading(false)
+  }
+  
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
+    setModal(false)
   }
 
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
   }
-
-  const sortPosts = (sort) => {
-    setSelectedSort(sort)
-  }
  
 return (
     <div className="App">
-      <PostForm create={createPost}/>
+      <MyButton onClick={fetchPosts}>get posts</MyButton>
+      <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+        Create user
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost}/>
+      </MyModal>
+      
       <hr style={{margin: '15px 0'}}/>
-      <div>
-        <MyInput
-          value={searchQuery}
-          onChange={e => setSearchQuery(e.target.value)}
-          placeholder='Search...'
-         />
-        <MySelect
-          value={selectedSort}
-          onChange={sortPosts} 
-          defaultValue='Sorting'
-          options={[
-            {value: 'title', name: 'By name'},
-            {value: 'body', name: 'By description'}
-          ]}
-        />
-      </div>
-      {sortedAndSearchedPosts.length 
-        ? <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Posts'/>
-        : <h2 style={{textAlign: 'center'}}>Posts not found</h2>
+      <PostFilter 
+        filter={filter}
+        setFilter={setFilter}
+      />
+      {isPostsLoading
+        ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader /></div>
+        : <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Posts'/>
       }
       
       <ClassCounter />
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import PostItem from './PostItem';
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
 
 const PostList = ({posts, title, remove}) => {
+
+    if (!posts.length) {
+        return (
+            <h2 style={{textAlign: 'center'}}>
+                Posts not found!
+            </h2>
+        )
+    }
+
     return (
         <div>
             <h1 style={{textAlign: 'center'}}>{title}</h1>
-            {posts.map((post, index) => 
-            <PostItem remove={remove} number={index + 1} post={post} key={post.id}/>
-            )} 
+            <TransitionGroup>
+                {posts.map((post, index) =>
+                    <CSSTransition 
+                        key={post.id}
+                        timeout={500}
+                        classNames="post"    
+                    >
+                        <PostItem remove={remove} number={index + 1} post={post} />
+                    </CSSTransition>
+                )}
+            </TransitionGroup>
         </div>
     );
 };
